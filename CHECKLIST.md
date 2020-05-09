aa5b44c883ae522d285bd485c490602e956ce144
Tan Nguyen <tannguyen1024@gmail.com>
2020-05-09 02:42:49 -0500

    Updated Checklist.md

--- CHECKLIST.md
+++ CHECKLIST.md
@@ -7,3 +7,6 @@
 6. Created folders and JSX files.
 7. HashRouter functionality between files.
 8. Set up Redux
+    a. Redux gathers data for smaller components.
+    b. Redux transmits all gathered feedback using reduxState to Review component.
+9. Set up alert/return when value is empty, less than 0, or greater than 5.
--- src/index.js
+++ src/index.js
@@ -15,35 +15,34 @@ let support;
 let comments;
 
 /* Create firstReducer Function */
-
 const firstReducer = (state = { feeling: '', understanding: '', support: '', review: '', comments: '' }, action) => {
     // console.log('(index.js) firstReducer loaded');
     if (action.type === 'Feeling') {
         console.log('Feeling set to:', action.payload)
-        console.log(`(index.js) state is:`, state)
+        // console.log(`(index.js) state is:`, state)
         feeling = action.payload.feeling;
         console.log('here is',feeling)
         return { ...state, feeling: action.payload.feeling };
     }
     if (action.type === 'Understanding') {
         console.log('Understanding set to:', action.payload)
-        console.log(`(index.js) state is:`, state)
+        // console.log(`(index.js) state is:`, state)
         understanding = action.payload.understanding;
         return { ...state, understanding: action.payload.understanding };
     }
     if (action.type === 'Support') {
         console.log('Support set to:', action.payload)
-        console.log(`(index.js) state is:`, state)
+        // console.log(`(index.js) state is:`, state)
         support = action.payload.support;
         return { ...state, support: action.payload.support };
     }
     if (action.type === 'Comments') {
         console.log('Comments set to:', action.payload)
-        console.log(`(index.js) state is:`, state)
+        // console.log(`(index.js) state is:`, state)
         comments = action.payload.comments;
         return { ...state, comments: action.payload.comments };
     }
-    console.log(`(index.js) state is:`,state)
+    // console.log(`(index.js) state is:`,state) /* No longer needed */
     return state;
 }
 
