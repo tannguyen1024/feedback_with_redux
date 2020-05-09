cea3229213e6acc57145fefa3b3402f91cb1b6fa
Tan Nguyen <tannguyen1024@gmail.com>
2020-05-08 22:06:59 -0500

    Created _template

---
+++ src/components/_template/_template.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NAME extends Component {
+
+    render(){
+        return(
+            <h1>NAME</h1>
+        )
+    }
+
+}
+
+export default NAME;
